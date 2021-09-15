2723f370851d771a2d8b96d508a670cfb1e9833f
David Sitrick <dsitrick@healthrecoverysolutions.com>
2021-09-15 03:03:38 -0400

    creating migration file for users table

--- project/database/migrations/2014_10_12_000000_create_users_table.php
+++ project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,16 +11,15 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->enum('user_type', ['player', 'coach'])->nullable()->default(null);
+            $table->string('first_name', 255)->nullable()->default(null);
+            $table->string('last_name', 255)->nullable()->default(null);
+            $table->unsignedTinyInteger('ranking')->nullable()->default('3');
+            $table->boolean('can_play_goalie')->nullable()->default('0');
         });
     }
 
@@ -29,7 +28,7 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
